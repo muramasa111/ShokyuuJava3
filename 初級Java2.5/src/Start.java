e18f6ad197bb7d6d1e810fbc1458ec70306fccc4
masaki murata <wiz.muramasa.111@gmail.com>
2024-05-13 11:54:59 +0900

    初級Java プロジェクトのコミット

---
+++ 初級Java2.5/src/Start.java
@@ -0,0 +1,22 @@
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		int x = 2, y = 3;
+		
+		System.out.println(x * y);
+		
+		/*MyFrame frame1 = new MyFrame();
+		frame1.setLocation(200,100);
+		frame1.setSize(500,500);*/
+		
+		MyFrame frame2 = new RectFrame();
+		frame2.setLocation(100,300);
+		
+		MyFrame frame3 = new RectFrame();
+		frame3.setLocation(50,300);
+	}
+
+}
