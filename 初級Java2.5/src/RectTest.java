01a256ac417f6557d7f166d84e82410c8d2f2a6a
masaki murata <wiz.muramasa.111@gmail.com>
2024-05-14 12:11:06 +0900

    3.2 様々な色で描く

--- 初級Java2.5/src/RectTest.java
+++ 初級Java2.5/src/RectTest.java
@@ -3,8 +3,19 @@ public class RectTest extends MyFrame{
 	
 	public void run() {
 		
-		fillRect(30,50,200,100);
+		setColor(255,255,0);		//黄
+		fillRect(30,40,50,100);	
 		
+		setColor(255,0,255);		//紫
+		fillRect(130,40,50,100);
 		
+		setColor(0,255,255);		//水
+		fillRect(230,40,50,100);
+		
+		setColor(0,0,0);			//黒
+		fillRect(330,40,50,100);
+		
+		setColor(128,128,128);		//灰
+		fillRect(30,200,150,10);
 	}
 }
