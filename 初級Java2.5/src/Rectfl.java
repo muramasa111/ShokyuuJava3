03468723bd1b337bdaa36159a9c839c4b73c0c38
masaki murata <wiz.muramasa.111@gmail.com>
2024-05-20 12:13:31 +0900

    確認問題4-4

--- 初級Java2.5/src/Rectfl.java
+++ 初級Java2.5/src/Rectfl.java
@@ -4,21 +4,21 @@ public class Rectfl extends MyFrame{
 	{
 		int x = 30;
 		setColor(0,128,0);
-		int i = 0;
+		int i;
 		for(i = 0; i <= 9; i++)
 		{
-			if( i== 4)
+			if( i == 0)
 			{
 				fillRect(x,100,10,100);
 			}
-			else if(i == 7)
-			{
-				fillRect(x,60,10,100);
-			}
-			else
+			else if(i == 9)
 			{
 				fillRect(x,80,10,100);
 
+			}
+			else
+			{
+				fillRect(x,90,10,100);
 			}
 			x+=20;
 		}
