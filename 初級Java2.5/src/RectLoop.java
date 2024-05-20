899d1e3ffd1a98a0cc0860975760980b3441818d
masaki murata <wiz.muramasa.111@gmail.com>
2024-05-20 11:43:42 +0900

    4.3 for文を利用する

--- 初級Java2.5/src/RectLoop.java
+++ 初級Java2.5/src/RectLoop.java
@@ -1,10 +1,11 @@
 
 public class RectLoop extends MyFrame{
 	public void run() {
-		int x = 30;
+		int x =30;
 		setColor(0,128,0);
-		while(x <= 170)
+		for(int i = 0; i <= 15; i++)
 		{
+			System.out.println(x);
 			fillRect(x,80,10,100);
 			x+=20;
 		}
