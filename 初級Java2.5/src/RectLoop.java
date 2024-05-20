e85cf7cbf60572385551b2fff19c751257ce6009
masaki murata <wiz.muramasa.111@gmail.com>
2024-05-20 11:45:33 +0900

    Revert "練習問題4-3"
    This reverts commit eec1db51662c8f23dfaa6ed3fc538c9f7e320448.

--- 初級Java2.5/src/RectLoop.java
+++ 初級Java2.5/src/RectLoop.java
@@ -3,7 +3,7 @@ public class RectLoop extends MyFrame{
 	public void run() {
 		int x =30;
 		setColor(0,128,0);
-		for(int i = 0; i <= 9; i++)
+		for(int i = 0; i <= 15; i++)
 		{
 			System.out.println(x);
 			fillRect(x,80,10,100);
