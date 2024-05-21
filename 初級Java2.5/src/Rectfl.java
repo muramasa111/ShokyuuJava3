fc9fb6badd6692e9676dbf87c9745a64957ad56b
masaki murata <wiz.muramasa.111@gmail.com>
2024-05-21 11:06:16 +0900

    練習問題4-ex2

--- 初級Java2.5/src/Rectfl.java
+++ 初級Java2.5/src/Rectfl.java
@@ -3,15 +3,16 @@ public class Rectfl extends MyFrame{
 	public void run()
 	{
 		int x = 20;
-		int y = 0;
+		int y = 180;
 		setColor(0,128,0);
 		int i = 0;
 		for(i = 0; i <= 9; i++)
 		{
-			fillRect(y,y,x,x);
-			y = y + x;
-			x += 20;
 			
+			fillRect(x,50,10,y - 50);
+			fillRect(x,y + 40,10,200 - y);
+			x += 20;
+			y -= 10;
 		}
 	}
 
